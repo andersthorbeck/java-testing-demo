a5e2e3b006984c00801241bc3618be458dabc0a2
Anders Rabo Thorbeck <andtho@knowit.no>
2020-01-06 17:34:35 +0100

    Adds test for celsiusToFahrenheit

---
+++ src/test/java/no/knowit/kds2020/climate/TemperatureConverterTest.java
@@ -0,0 +1,18 @@
+package no.knowit.kds2020.climate;
+
+import static no.knowit.kds2020.climate.TemperatureConverter.celsiusToFahrenheit;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TemperatureConverterTest {
+
+  private static final double ACCEPTABLE_DELTA = 0.0001;
+
+  @Test
+  public void celsiusToFahrenheit_should_convert_to_expected_fahrenheit_value() {
+    assertEquals(32.0, celsiusToFahrenheit(0), ACCEPTABLE_DELTA);
+    assertEquals(212.0, celsiusToFahrenheit(100), ACCEPTABLE_DELTA);
+    assertEquals(-148.0, celsiusToFahrenheit(-100), ACCEPTABLE_DELTA);
+  }
+}
