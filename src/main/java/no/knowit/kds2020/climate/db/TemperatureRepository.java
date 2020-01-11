58e8e7f1dec58ee9fdb5f9ec0d4241b0f359940b
Anders Rabo Thorbeck <andtho@knowit.no>
2020-01-11 10:29:54 +0100

    Adds Spring Boot dependency for JDBC template

--- src/main/java/no/knowit/kds2020/climate/db/TemperatureRepository.java
+++ src/main/java/no/knowit/kds2020/climate/db/TemperatureRepository.java
@@ -2,17 +2,9 @@ package no.knowit.kds2020.climate.db;
 
 import javax.sql.DataSource;
 import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class TemperatureRepository {
-
-  private final DataSource dataSource = createDataSource();
-
-  private static DataSource createDataSource() {
-    PGSimpleDataSource dataSource = new PGSimpleDataSource();
-    dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
-    dataSource.setUser("postgres");
-    dataSource.setPassword("password");
-    return dataSource;
-  }
 
 }
