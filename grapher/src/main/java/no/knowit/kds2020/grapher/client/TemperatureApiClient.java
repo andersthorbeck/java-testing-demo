9682088cbae9c70b7d8fe2a9482616d4a4343a71
Anders Rabo Thorbeck <andtho@knowit.no>
2020-01-12 21:10:28 +0100

    grapher: Parameterizes base URL

--- grapher/src/main/java/no/knowit/kds2020/grapher/client/TemperatureApiClient.java
+++ grapher/src/main/java/no/knowit/kds2020/grapher/client/TemperatureApiClient.java
@@ -3,6 +3,7 @@ package no.knowit.kds2020.grapher.client;
 import java.util.List;
 import no.knowit.kds2020.grapher.model.TemperatureReading;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -12,18 +13,21 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class TemperatureApiClient {
 
-  private static final String apiUrl = "http://localhost:8080/temperature/readings/all";
-
+  private final String apiBaseUrl;
   private final RestTemplate restTemplate;
 
   @Autowired
-  public TemperatureApiClient(RestTemplate restTemplate) {
+  public TemperatureApiClient(
+      @Value("http://localhost:8080") String apiBaseUrl,
+      RestTemplate restTemplate
+  ) {
     this.restTemplate = restTemplate;
+    this.apiBaseUrl = apiBaseUrl;
   }
 
   public List<TemperatureReading> getAllTemperatureReadings() {
     ResponseEntity<List<TemperatureReading>> responseEntity = restTemplate.exchange(
-        apiUrl,
+        apiBaseUrl + "/temperature/readings/all",
         HttpMethod.GET,
         null,
         new ParameterizedTypeReference<List<TemperatureReading>>() {}
