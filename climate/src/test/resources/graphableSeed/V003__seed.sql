311795ec9cd8dbf89eafa7f7f8d263ae95cd9161
Anders Rabo Thorbeck <andtho@knowit.no>
2020-01-17 17:51:25 +0100

    Adds graphableSeed directory for populating test DB

---
+++ climate/src/test/resources/graphableSeed/V003__seed.sql
@@ -0,0 +1,4 @@
+INSERT into TEMPERATURE (datetime, celsius) VALUES (now() - interval '3 hours', 3);
+INSERT into TEMPERATURE (datetime, celsius) VALUES (now() - interval '2 hours', -5);
+INSERT into TEMPERATURE (datetime, celsius) VALUES (now() - interval '1 hours', 0);
+INSERT into TEMPERATURE (datetime, celsius) VALUES (now(), 13);
