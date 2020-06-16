31191cbf2a0afa9e35d31a2ede4e39304b35ee9f
Anders Rabo Thorbeck <andtho@knowit.no>
2020-06-16 20:52:01 +0200

    Mocks repository in TemperatureControllerWebTest

--- climate/src/test/java/no/knowit/kds2020/climate/api/TemperatureControllerWebTest.java
+++ climate/src/test/java/no/knowit/kds2020/climate/api/TemperatureControllerWebTest.java
@@ -3,49 +3,35 @@ package no.knowit.kds2020.climate.api;
 import static org.mockito.Mockito.verify;
 
 import java.util.List;
-import javax.sql.DataSource;
+import no.knowit.kds2020.climate.db.TemperatureRepository;
 import no.knowit.kds2020.climate.model.TemperatureReading;
 import no.knowit.kds2020.climate.service.TemperatureService;
-import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.testcontainers.containers.PostgreSQLContainer;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 public class TemperatureControllerWebTest {
-
-  /** Note: shouldn't need this, but couldn't figure out how to
-    * only load top layer of application context. */
-  @ClassRule
-  public static PostgreSQLContainer postgres = new PostgreSQLContainer();
-
-  @TestConfiguration
-  static class LocalConfig {
-    @Bean
-    public DataSource dataSource() {
-      return DataSourceBuilder.create()
-          .url(postgres.getJdbcUrl())
-          .username(postgres.getUsername())
-          .password(postgres.getPassword())
-          .build();
-    }
-  }
 
   @Autowired
   private TestRestTemplate restTemplate;
 
   @MockBean
   private TemperatureService serviceMock;
+
+  /** Note: should ideally not have to explicitly mock the repository layer,
+    * when already mocking the service layer. */
+  @MockBean
+  private TemperatureRepository repositoryMock;
 
   @Test
   public void get_against_temperature_readings_all_path_should_call_getAllTemperatureReadings() {
