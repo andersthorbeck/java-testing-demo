a33ff6eebd49b260fb1ccaabd77c4e1c1ff67ebf
Anders Rabo Thorbeck <andtho@knowit.no>
2020-06-18 01:12:29 +0200

    Replaces explicit local @TestConfiguration with @ActiveProfiles
    … for brevity.

--- climate/src/test/java/no/knowit/kds2020/climate/endtoend/TemperatureControllerEndToEndTest.java
+++ climate/src/test/java/no/knowit/kds2020/climate/endtoend/TemperatureControllerEndToEndTest.java
@@ -7,7 +7,6 @@ import static org.hamcrest.Matchers.is;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import javax.sql.DataSource;
 import no.knowit.kds2020.climate.model.TemperatureReading;
 import org.flywaydb.test.FlywayTestExecutionListener;
 import org.flywaydb.test.annotation.FlywayTest;
@@ -15,15 +14,13 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.TestExecutionListeners.MergeMode;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -35,22 +32,11 @@ import org.testcontainers.containers.PostgreSQLContainer;
     listeners = FlywayTestExecutionListener.class,
     mergeMode = MergeMode.MERGE_WITH_DEFAULTS
 )
+@ActiveProfiles("db-test")
 public class TemperatureControllerEndToEndTest {
 
   @ClassRule
   public static PostgreSQLContainer postgres = new PostgreSQLContainer();
-
-  @TestConfiguration
-  static class LocalConfig {
-    @Bean
-    public DataSource dataSource() {
-      return DataSourceBuilder.create()
-          .url(postgres.getJdbcUrl())
-          .username(postgres.getUsername())
-          .password(postgres.getPassword())
-          .build();
-    }
-  }
 
   @Autowired
   private TestRestTemplate restTemplate;
